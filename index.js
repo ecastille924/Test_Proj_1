ebe8ba92c3da88dcb619d43121f105008726acd7
Erick Castille <ecastille924@gmail.com>
2022-01-22 16:28:42 -0500

    adding functions for all buttons

---
+++ index.js
@@ -0,0 +1,17 @@
+function rotateOnClick(){
+    let guitarPng = document.getElementById("guitar-png")
+    guitarPng.className = ""
+    guitarPng.className = "guitar"
+}
+
+function zoomOnClick(){
+    let guitarPng = document.getElementById("guitar-png")
+    guitarPng.className = ""
+    guitarPng.className = "guitar2"
+}
+
+function perspectiveOnClick(){
+    let guitarPng = document.getElementById("guitar-png")
+    guitarPng.className = ""
+    guitarPng.className = "guitar3"
+}
